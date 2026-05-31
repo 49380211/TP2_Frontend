e2660fc369f06f06a6fd8bd18b25107b6f809676
49380211 <49380211@est.ort.edu.ar>
2026-05-31 15:07:54 -0300

    vs

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import ws from 'ws';
 
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  realtime: {
+    transport: ws,
+  },
+});
